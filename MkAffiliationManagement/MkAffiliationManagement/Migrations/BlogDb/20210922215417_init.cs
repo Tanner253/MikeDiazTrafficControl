69e9ba621210d8392c06f32c92564468ab81a0de
Tanner Percival <percivaltanner@gmail.com>
2021-09-22 14:58:14 -0700

    model first database creation need to do controllers and repository design pattern

---
+++ MkAffiliationManagement/MkAffiliationManagement/Migrations/BlogDb/20210922215417_init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MkAffiliationManagement.Migrations.BlogDb
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blog",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Body = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Blog",
+                columns: new[] { "ID", "Body", "Image", "Title" },
+                values: new object[] { 1, "This is the Body", "", "This is the Title." });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blog");
+        }
+    }
+}
