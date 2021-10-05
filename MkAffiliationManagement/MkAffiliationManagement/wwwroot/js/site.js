d9a71f93916d5a6b5cef7af7cf6a83b2ad3514b1
Tanner Percival <percivaltanner@gmail.com>
2021-10-05 13:22:25 -0700

    Big UI changes

--- MkAffiliationManagement/MkAffiliationManagement/wwwroot/js/site.js
+++ MkAffiliationManagement/MkAffiliationManagement/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
